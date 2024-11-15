5aa2cdb5382219a85845e5778e50d069f3df644f
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2024-11-15 14:45:54 +0100

    animation

--- main.ts
+++ main.ts
@@ -4,4 +4,6 @@ radio.setTransmitPower(6)
 basic.forever(function () {
     radio.sendString("A")
     basic.pause(3000)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
 })
