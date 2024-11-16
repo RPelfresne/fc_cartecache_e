6a808b2b066d0d43fc4c890d147213df4f725185
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2024-11-16 18:08:08 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ radio.setGroup(1)
 radio.setTransmitPower(6)
 basic.forever(function () {
     radio.sendString("A")
-    basic.pause(3000)
     basic.showIcon(IconNames.Diamond)
     basic.showIcon(IconNames.SmallDiamond)
 })
